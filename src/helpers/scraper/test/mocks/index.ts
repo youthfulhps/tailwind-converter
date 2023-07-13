bb666251b24be4ba92f7e7ce1a6368bb3c159aa5
youthfulhps <ybh942002@gmail.com>
2023-07-14 01:47:00 +0900

    test: Add mock data for scraper test

---
+++ src/helpers/scraper/test/mocks/index.ts
@@ -0,0 +1,25 @@
+export const stringifiedTestComponent =
+  "import React from 'react';\n" +
+  "import styled from 'styled-components';\n" +
+  '\n' +
+  'const StyledButton = styled.button`\n' +
+  '  display: flex;\n' +
+  '  justify-content: center;\n' +
+  '  align-items: center;\n' +
+  '`;\n' +
+  '\n' +
+  'const StyledText = styled.span`\n' +
+  '  font-size: 8px;\n' +
+  '`;\n' +
+  '\n' +
+  'function TestComponent() {\n' +
+  '  return (\n' +
+  '    <StyledButton>\n' +
+  '      <StyledText>테스트</StyledText>\n' +
+  '    </StyledButton>\n' +
+  '  );\n' +
+  '}\n' +
+  '\n' +
+  'export default TestComponent;\n';
+
+Number.MAX_SAFE_INTEGER;
