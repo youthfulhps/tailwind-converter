99011389e20cb918fc37b5b909cab5f428c39715
youthfulhps <ybh942002@gmail.com>
2023-08-31 19:20:12 +0900

    enhance: Define plugin with customized printer and parser

---
+++ src/plugin.ts
@@ -0,0 +1,23 @@
+import { parsers as babelParsers } from 'prettier/parser-babel';
+import { parsers as typescriptParsers } from 'prettier/parser-typescript';
+
+import { printers } from './helpers/printer';
+
+export const parsers: { [parserName: string]: any } = {
+  babel: {
+    ...babelParsers.babel,
+    astFormat: 'babel-ast',
+  },
+  typescript: {
+    ...typescriptParsers.typescript,
+    astFormat: 'typescript-ast',
+  },
+};
+
+const plugin = {
+  parsers,
+  printers,
+  astFormat: 'estree',
+};
+
+export default plugin;
