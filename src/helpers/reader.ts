81debb2dc319781ca313cbdc8eeb62b79ca8a77e
youthfulhps <ybh942002@gmail.com>
2023-09-01 16:11:47 +0900

    feature: Implement function to write file containing formatted script

--- src/helpers/reader.ts
+++ src/helpers/reader.ts
@@ -4,3 +4,7 @@ export async function scrapRawScript(target: string) {
   const response = await fs.readFile(target, 'utf-8');
   return response;
 }
+
+export async function writeFormattedScript(target: string, script: string) {
+  await fs.writeFile(target, script);
+}
