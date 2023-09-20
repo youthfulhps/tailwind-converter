4a33378c631b566848c97fe719069709492b67c6
youthfulhps <ybh942002@gmail.com>
2023-09-20 15:38:13 +0900

    chore: Replace node-sass with sass dependency

--- src/helpers/css-parser.ts
+++ src/helpers/css-parser.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const sass = require('node-sass');
+const sass = require('sass');
 const css = require('css');
 
 export function parseSass(rawScript: string) {
-  const parsedSass = sass.renderSync({
-    data: '_ {' + rawScript + '}',
-  });
+  const parsedSass = sass.compileString(`_ { ${rawScript} }`);
   const cssAst = css.parse(parsedSass.css.toString());
 
   return cssAst.stylesheet.rules;
